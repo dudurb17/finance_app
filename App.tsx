c265e4141d248cacc94a7341f54dadd2ae812320
Eduardo <eduardo.robettibedin@gmail.com>
2026-01-02 21:51:07 -0300

    refactor(App): - update layout and styling for main view

--- App.tsx
+++ App.tsx
@@ -9,15 +9,24 @@ import styled from 'styled-components/native';
 function App() {
   return (
     <SafeAreaProvider>
-       <View >
-        <Text>Hello World</Text>
-       </View>
+       <Container >
+        <Title>Hello World</Title>
+       </Container>
     </SafeAreaProvider>
   );
 }
 
+const Container = styled.View`
+flex:1;
+justify-content: center;
+align-items: center;
+background-color: #000;
+`;
 
-
-
+const Title = styled.Text`
+font-size: 20px;
+font-weight: bold;
+color: #fff;
+`;
 
 export default App;
