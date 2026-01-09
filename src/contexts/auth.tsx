8972130e79cf286a799bd317bb27bd1623d10615
Eduardo <eduardo.robettibedin@gmail.com>
2026-01-09 00:43:58 -0300

    feat: implement user registration functionality in Auth context and SignUp component
    - Add signUp method to AuthContext for handling user registration
    - Update user state to include email and password fields
    - Refactor SignUp component to manage user input and trigger signUp on form submission
    - Enhance input fields for name, email, and password in SignUp form

--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -1,21 +1,36 @@
 import { createContext, useContext, useState } from 'react';
 import { User } from '@/types/user';
+import api from '@/services/api';
+import { useNavigation } from '@react-navigation/native';
+import { PublicRoutesNavigationProp } from '@/routes/public/types';
 
 export const AuthContext = createContext<{
   user: User;
   setUser: (user: User) => void;
+  signUp: (user: User) => Promise<void>;
 }>({
-  user: { name: '' },
+  user: { name: '', email: '' },
   setUser: () => {},
+  signUp: () => Promise.resolve(),
 });
 
 function AuthProvider({ children }: { children: React.ReactNode }) {
   const [user, setUser] = useState<User>({
     name: 'Eduardo',
+    email: 'eduardo@gmail.com',
   });
 
+  async function signUp(user: User) {
+    try {
+      const response = await api.post('/users', user);
+      console.log('response', response);
+    } catch (error) {
+      console.error('error', error);
+    }
+  }
+
   return (
-    <AuthContext.Provider value={{ user, setUser }}>
+    <AuthContext.Provider value={{ user, setUser, signUp }}>
       {children}
     </AuthContext.Provider>
   );
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -7,31 +7,56 @@ import {
   SubmitButton,
   SubmitText,
 } from './styles';
+import { useState } from 'react';
+import { useAuth } from '@/contexts/auth';
+import { useNavigation } from '@react-navigation/native';
+import { PublicRoutesNavigationProp } from '@/routes/public/types';
 
 export default function SignUp() {
+  const { signUp } = useAuth();
+  const navigation = useNavigation<PublicRoutesNavigationProp>();
+  const [user, setUser] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+
+  async function handleSignUp() {
+    await signUp(user);
+    navigation.goBack();
+  }
+
   return (
     <Container removedPaddingTop={true}>
       <SignUpContent>
         <AreaInput>
-          <Input placeholder="Nome" placeholderTextColor="#CCCCCC" />
-        </AreaInput>
-
-        <AreaInput>
-          <Input placeholder="Seu email" placeholderTextColor="#CCCCCC" />
-        </AreaInput>
-
-        <AreaInput>
-          <Input placeholder="Sua senha" placeholderTextColor="#CCCCCC" />
-        </AreaInput>
-
-        <AreaInput>
           <Input
-            placeholder="Confirme sua senha"
+            value={user?.name}
+            onChangeText={text => setUser({ ...user, name: text })}
+            placeholder="Nome"
             placeholderTextColor="#CCCCCC"
           />
         </AreaInput>
 
-        <SubmitButton>
+        <AreaInput>
+          <Input
+            value={user?.email}
+            onChangeText={text => setUser({ ...user, email: text })}
+            placeholder="Seu email"
+            placeholderTextColor="#CCCCCC"
+          />
+        </AreaInput>
+
+        <AreaInput>
+          <Input
+            value={user?.password}
+            onChangeText={text => setUser({ ...user, password: text })}
+            placeholder="Sua senha"
+            placeholderTextColor="#CCCCCC"
+          />
+        </AreaInput>
+
+        <SubmitButton onPress={handleSignUp}>
           <SubmitText>Cadastrar</SubmitText>
         </SubmitButton>
       </SignUpContent>
