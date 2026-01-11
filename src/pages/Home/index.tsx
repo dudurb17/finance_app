02f4a7591a09dca61298c36aa72f0179504711b1
Eduardo <eduardo.robettibedin@gmail.com>
2026-01-10 22:26:34 -0300

    feat: add date-fns for date formatting and implement balance fetching in Home component
    - Introduce date-fns dependency for date manipulation
    - Enhance Home component to fetch and display balance data based on selected date
    - Implement useEffect to manage API calls and state updates for balance list

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,39 @@
 import { Text } from 'react-native';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Container from '@/components/Container';
 import { useAuth } from '@/contexts/auth';
 import Button from '@/components/Button';
+import api from '@/services/api';
+import { format } from 'date-fns';
 
 export default function Home() {
   const { signOut } = useAuth();
+  const [listBalences, setListBalences] = useState([]);
+  const [dateMovements, setDateMovements] = useState(new Date());
+
+  useEffect(() => {
+    let isActive = true;
+
+    async function getListBalences() {
+      try {
+        let date = format(dateMovements, 'yyyy-MM-dd');
+        const response = await api.get('/balance', { params: { date } });
+        if (isActive) {
+          setListBalences(response?.data);
+        }
+        console.log('response', response?.data);
+      } catch (error) {
+        console.error('error', error);
+      }
+    }
+
+    getListBalences();
+
+    return () => {
+      isActive = false;
+    };
+  }, []);
+
   return (
     <Container removedPaddingTop>
       <Text>Home</Text>
