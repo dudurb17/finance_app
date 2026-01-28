e674a350e06c7f9fc78e446ea6bb5ef9eb3b6e74
Eduardo <eduardo.robettibedin@gmail.com>
2026-01-28 13:03:12 -0300

    refactor: update VSCode settings and enhance MovementItem component layout
    - Change default formatter in VSCode settings to Prettier for consistent code formatting
    - Refactor MovementItem component for improved readability and maintainability
    - Adjust layout and styling of MovementItem to enhance visual presentation of movement details

--- src/components/MovementItem/index.tsx
+++ src/components/MovementItem/index.tsx
@@ -9,14 +9,26 @@ interface MovementItemProps {
 }
 
 export default function MovementItem({ movement }: MovementItemProps) {
-  return (<View className="bg-gray-200 rounded-lg p-2 justify-center">
-    <View className={cn(
-      "flex-row items-center justify-center w-25 gap-2 rounded-lg h-8",
-      movement?.type === 'receita' ? 'bg-green-500' : 'bg-red-500',
-    )}>
-      <FontAwesome name={movement?.type === 'receita' ? 'arrow-up' : 'arrow-down'} size={20} color="white" />
-      <Text className="text-sm font-bold text-white">{movement?.type === 'receita' ? 'Receita' : 'Despesa'}</Text>
+  return (
+    <View className="bg-gray-200 rounded-lg p-2 justify-center">
+      <View
+        className={cn(
+          'flex-row items-center justify-center w-25 gap-2 rounded-lg h-8',
+          movement?.type === 'receita' ? 'bg-green-500' : 'bg-red-500',
+        )}
+      >
+        <FontAwesome
+          name={movement?.type === 'receita' ? 'arrow-up' : 'arrow-down'}
+          size={20}
+          color="white"
+        />
+        <Text className="text-sm font-bold text-white">
+          {movement?.type === 'receita' ? 'Receita' : 'Despesa'}
+        </Text>
+      </View>
+      <Text className="text-xl font-bold">
+        R$ {movement?.value?.toFixed(2).replace('.', ',')}
+      </Text>
     </View>
-    <Text className="text-xl font-bold">R$ {movement?.value?.toFixed(2).replace('.', ',')}</Text></View>
   );
 }
